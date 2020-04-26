c915cf186ff21514afa24d81255f91d61689821c
george <georgios.perivolarakis@gamelounge.com>
2020-04-26 21:03:39 +0200

    fix: added auth0 for prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,9 @@
 export const environment = {
-  production: true
+  production: true,
+  auth0: {
+    url: 'drnio13.eu', // the auth0 domain prefix
+    audience: 'pets', // the audience set for the auth0 app
+    clientId: 'YE2nSEZRU0gtIAU0bG87N8vqRFv1U9s3', // the client id generated for the auth0 app
+    callbackURL: 'https://pet-rescue-center-app.herokuapp.com', // the base url of the running ionic application.
+  }
 };
